05fc195889fc70c4e04f6797863815d8b9a6b5a7
陈刚 <chengang@chengangdeMacBook-Pro-2.local>
2024-05-27 15:19:52 +0800

    base

--- demo/webpack-demo/config/webpack.prod.js
+++ demo/webpack-demo/config/webpack.prod.js
@@ -1,25 +1,27 @@
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 const webpack = require('webpack');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   output: {
-    filename: 'bundle.[contenthash:8].js',
+    clean: true, // 每次构建前清除输出目录  webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    filename: '[name].[contenthash:8].js', // contenthash依据文件内容来，文件内容改变hash值才会改变
   },
   module: {
     rules: [
       {
         test: /\.(png|jpg|jpeg|git)$/,
-        use: [
-          'url-loader',
-          {
-            options: {
-              limit: 5 * 1024,
-              outputPath: '/image/',
-            },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
           },
-        ],
+        },
+        generator: {
+          filename: 'images/[contenthash:8][ext]', // 文件输出目录和命名
+        },
       },
     ],
   },
@@ -28,4 +30,33 @@ module.exports = merge(baseConfig, {
       'window.ENV': JSON.stringify('production'),
     }),
   ],
+  // 压缩配置
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(), //压缩js
+      new CssMinimizerPlugin(), // 压缩css
+    ],
+    splitChunks: {
+      // chunks: 'initial', //入口chunks，对于异步导入的文件不处理
+      // chunks: 'async', //异步chunks，只对异步导入的文件做处理
+      chunks: 'all', //全部处理
+      cacheGroups: {
+        // 第三方包
+        vendor: {
+          name: 'vendor', // chunk的名字
+          priority: 1, //配置抽离的权重，权重越高 越先抽离
+          minSize: 0, //大小限制 达到这个权重才会进行差分
+          minChunks: 1, // 最少复用过几次
+        },
+        // 公共模块
+        commons: {
+          name: 'commons', // chunk的名字
+          priority: 0, //配置抽离的权重，权重越高 越先抽离
+          minSize: 0, //大小限制 达到这个权重才会进行差分
+          minChunks: 2, // 最少复用过几次
+        },
+      },
+    },
+  },
 });
--- demo/webpack-demo/src/index.ts
+++ demo/webpack-demo/src/index.ts
@@ -1,4 +1,11 @@
+import testImg from '@/img/1.png';
 import './index.css';
 if (window.ENV === 'development') {
   console.log('ok');
 }
+const img = document.createElement('img');
+img.src = testImg;
+img.style.width = '100px';
+img.style.height = '100px';
+
+document.querySelector('body')?.appendChild(img);
