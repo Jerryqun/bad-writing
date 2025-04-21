e63202092e6c8eb573b4d83ed626f81452a2225a
cq <wb-zcq621102@alibaba-inc.com>
2025-04-21 09:56:08 +0800

    fix

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,7 +8,7 @@ export default {
     name: '编程小径',
     logo: 'https://wisdomhammer.oss-cn-hangzhou.aliyuncs.com/pmsaas/1025/1/20230721/02be82d4e18bc3e7e155e1f744cf4586.jpeg?Expires=3267854213&OSSAccessKeyId=LTAI5t5fwTBbu4WT1PJCaeCS&Signature=MM2nS5lj6%2FEr7X%2FKkbeEP15bKsc%3D',
     socialLinks: {
-      facebook: 'http://112.74.190.11:9999/#/~demos/docs-playground',
+      github: 'https://github.com/Jerryqun/bad-writing',
     },
   },
 };
