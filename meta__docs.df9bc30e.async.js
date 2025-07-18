2da1c911be957c98745dffe6c686f226755d333d
Jerryqun <Jerryqun@users.noreply.github.com>
2025-07-18 07:21:28 +0000

    deploy: d8acf80e030daa6eff1a0d89642532163cc82d88

--- meta__docs.df9bc30e.async.js
+++ meta__docs.df9bc30e.async.js
@@ -7004,16 +7004,47 @@ fileInput.onchange = (e) => {
     e.target.files[0],
   );
 };
-`,paraId:16,tocIndex:5}]},34646:function(o,n,e){e.r(n),e.d(n,{texts:function(){return a}});const a=[{value:"\u5FAE\u4EFB\u52A1\u6267\u884C\u65E9\u4E8E\u5B8F\u4EFB\u52A1",paraId:0,tocIndex:0},{value:"\u5FAE\u4EFB\u52A1\u662F\u5728\u4E0B\u4E00\u8F6E dom \u6E32\u67D3\u4E4B\u524D\u6267\u884C\uFF0C\u5B8F\u4EFB\u52A1\u662F\u4E0B\u4E00\u8F6E dom \u6E32\u67D3\u4E4B\u540E\u6267\u884C",paraId:1,tocIndex:0},{value:"\u5B8F\u4EFB\u52A1 =\u300B \u5FAE\u4EFB\u52A1=\u300Bdom \u6E32\u67D3 =\u300B\u5B8F\u4EFB\u52A1",paraId:2,tocIndex:0},{value:"setTimeout(fn,1000) fn \u51FD\u6570\u6253\u5165\u5B8F\u4EFB\u52A1\u961F\u5217\u7684\u65F6\u673A\u662F 1s \u540E\u800C\u4E0D\u662F setTimeout(fn,1000) \u7684\u6267\u884C\u65F6\u673A",paraId:3,tocIndex:0},{value:"script \uFF08\u4E3B\u4EE3\u7801\u5757\uFF09\u3001setTimeout\u3001 setInterval \u3001setImmediate\u3001 I/O \u3001UI rendering",paraId:4,tocIndex:1},{value:"process.nextTick\uFF08Nodejs\uFF09 \u3001promise then \u91CC\u9762\u7684 js \u4EE3\u7801 \u3001async await (await \u540E\u9762\u7684 js \u4EE3\u7801) \u3001 Object.observe \u3001MutationObserver",paraId:5,tocIndex:2},{value:"\u5B8F\u4EFB\u52A1 => \u5FAE\u4EFB\u52A1\u7684 Event Queue => \u5B8F\u4EFB\u52A1\u7684 Event Queue",paraId:6,tocIndex:2},{value:"\u6817\u5B50",paraId:7,tocIndex:3},{value:`setTimeout(() => {
+`,paraId:16,tocIndex:5}]},34646:function(o,n,e){e.r(n),e.d(n,{texts:function(){return a}});const a=[{value:"\u5FAE\u4EFB\u52A1\u6267\u884C\u65E9\u4E8E\u5B8F\u4EFB\u52A1",paraId:0,tocIndex:0},{value:"\u5FAE\u4EFB\u52A1\u662F\u5728\u4E0B\u4E00\u8F6E dom \u6E32\u67D3\u4E4B\u524D\u6267\u884C\uFF0C\u5B8F\u4EFB\u52A1\u662F\u4E0B\u4E00\u8F6E dom \u6E32\u67D3\u4E4B\u540E\u6267\u884C",paraId:1,tocIndex:0},{value:"\u5B8F\u4EFB\u52A1 => \u5FAE\u4EFB\u52A1 => dom \u6E32\u67D3 => \u5B8F\u4EFB\u52A1",paraId:2,tocIndex:0},{value:"setTimeout(fn,1000) fn \u51FD\u6570\u6253\u5165\u5B8F\u4EFB\u52A1\u961F\u5217\u7684\u65F6\u673A\u662F 1s \u540E\u800C\u4E0D\u662F setTimeout(fn,1000) \u7684\u6267\u884C\u65F6\u673A",paraId:3,tocIndex:0},{value:"\u603B\u7ED3\uFF1A",paraId:4,tocIndex:0},{value:"\u6267\u884C\u540C\u6B65\u4EE3\u7801\uFF0C\u8FD9\u662F\u5B8F\u4EFB\u52A1",paraId:5,tocIndex:0},{value:"\u6267\u884C\u6808\u4E3A\u7A7A \u67E5\u8BE2\u662F\u5426\u6709\u9700\u8981\u6267\u884C\u7684\u5FAE\u4EFB\u52A1",paraId:5,tocIndex:0},{value:"\u6267\u884C\u6240\u6709\u7684\u5FAE\u4EFB\u52A1",paraId:5,tocIndex:0},{value:"\u5FC5\u8981\u7684\u8BDD\u6E32\u67D3UI",paraId:5,tocIndex:0},{value:"\u5F00\u542F\u4E0B\u4E00\u8F6Eevent loop\uFF0C\u6267\u884C\u5B8F\u4EFB\u52A1\u4E2D\u7684\u5F02\u6B65\u4EE3\u7801",paraId:5,tocIndex:0},{value:`
+\u901A\u8FC7\u4E0A\u8FF0\u7684 Event loop \u987A\u5E8F\u53EF\u77E5\uFF0C\u5982\u679C\u5B8F\u4EFB\u52A1\u4E2D\u7684\u5F02\u6B65\u4EE3\u7801\u6709\u5927\u91CF\u7684\u8BA1\u7B97\u5E76\u4E14\u9700\u8981\u64CD\u4F5C DOM \u7684\u8BDD\uFF0C\u4E3A\u4E86\u66F4\u5FEB\u7684\u54CD\u5E94\u754C\u9762\u54CD\u5E94\uFF0C\u6211\u4EEC\u53EF\u4EE5\u628A\u64CD\u4F5C DOM \u653E\u5165\u5FAE\u4EFB\u52A1\u4E2D`,paraId:5,tocIndex:0},{value:"script \uFF08\u4E3B\u4EE3\u7801\u5757\uFF09\u3001setTimeout\u3001 setInterval \u3001setImmediate\u3001 I/O \u3001UI rendering",paraId:6,tocIndex:1},{value:"process.nextTick\uFF08Nodejs\uFF09 \u3001promise then \u91CC\u9762\u7684 js \u4EE3\u7801 \u3001async await (await \u540E\u9762\u7684 js \u4EE3\u7801) \u3001 Object.observe \u3001MutationObserver",paraId:7,tocIndex:2},{value:"\u5B8F\u4EFB\u52A1 => \u5FAE\u4EFB\u52A1\u7684 Event Queue => \u5B8F\u4EFB\u52A1\u7684 Event Queue",paraId:8,tocIndex:2},{value:"\u6817\u5B50",paraId:9,tocIndex:3},{value:`setTimeout(() => {
   document.querySelector('body').style.backgroundColor = 'green';
 }, 2000);
 document.querySelector('body').style.backgroundColor = 'red';
 // \u8868\u73B0\u4E3A\u5148\u53D8\u7EA2 \u4E24\u79D2\u540E\u53D8\u7EFF
-`,paraId:8,tocIndex:3},{value:"MutationObserver",paraId:9,tocIndex:4},{value:" \u662F\u4E00\u4E2A Web API\uFF0C\u7528\u4E8E\u76D1\u89C6 DOM \u6811\u7684\u53D8\u5316\uFF0C\u5982\u5143\u7D20\u7684\u6DFB\u52A0\u6216\u5220\u9664\u3001\u5C5E\u6027\u7684\u53D8\u66F4\u7B49\u3002\u5F53\u8FD9\u4E9B\u53D8\u5316\u53D1\u751F\u65F6\uFF0C",paraId:9,tocIndex:4},{value:"MutationObserver",paraId:9,tocIndex:4},{value:" \u53EF\u4EE5\u5F02\u6B65\u5730\u901A\u77E5\u5F00\u53D1\u8005\uFF0C\u5141\u8BB8\u4ED6\u4EEC\u636E\u6B64\u6267\u884C\u76F8\u5173\u7684\u56DE\u8C03\u51FD\u6570\u3002\u8FD9\u79CD\u673A\u5236\u53EF\u4EE5\u7528\u4E8E\u6267\u884C\u5404\u79CD\u4EFB\u52A1\uFF0C\u6BD4\u5982\u8DDF\u8E2A DOM \u7684\u53D8\u5316\u6765\u540C\u6B65\u66F4\u65B0 UI \u7EC4\u4EF6\u6216\u8005\u6267\u884C\u67D0\u4E9B\u7279\u5B9A\u7684\u903B\u8F91\u3002",paraId:9,tocIndex:4},{value:"MutationObserver",paraId:10,tocIndex:4},{value:" \u7684\u4F7F\u7528\u6B65\u9AA4\u5982\u4E0B\uFF1A",paraId:10,tocIndex:4},{value:"\u521B\u5EFA\u4E00\u4E2A\u89C2\u5BDF\u8005\u5B9E\u4F8B",paraId:11,tocIndex:4},{value:`\uFF1A
-\u4F7F\u7528 `,paraId:11,tocIndex:4},{value:"MutationObserver",paraId:11,tocIndex:4},{value:" \u6784\u9020\u51FD\u6570\u521B\u5EFA\u4E00\u4E2A\u65B0\u7684\u89C2\u5BDF\u8005\u5B9E\u4F8B\uFF0C\u4F20\u9012\u4E00\u4E2A\u56DE\u8C03\u51FD\u6570\u7ED9\u5B83\uFF0C\u8BE5\u51FD\u6570\u5C06\u5728\u6BCF\u6B21 DOM \u53D8\u5316\u65F6\u88AB\u8C03\u7528\u3002",paraId:11,tocIndex:4},{value:"\u914D\u7F6E\u89C2\u5BDF\u8005",paraId:12,tocIndex:4},{value:`\uFF1A
-\u901A\u8FC7\u8C03\u7528 `,paraId:12,tocIndex:4},{value:"observer.observe(targetNode, config)",paraId:12,tocIndex:4},{value:" \u65B9\u6CD5\u6765\u914D\u7F6E\u89C2\u5BDF\u8005\uFF0C\u6307\u5B9A\u4F60\u60F3\u8981\u76D1\u89C6\u7684 DOM \u8282\u70B9 (",paraId:12,tocIndex:4},{value:"targetNode",paraId:12,tocIndex:4},{value:") \u4EE5\u53CA\u4F60\u611F\u5174\u8DA3\u7684\u7279\u5B9A\u7C7B\u578B\u7684 DOM \u53D8\u52A8 (",paraId:12,tocIndex:4},{value:"config",paraId:12,tocIndex:4},{value:")\u3002",paraId:12,tocIndex:4},{value:"\u54CD\u5E94 DOM \u53D8\u5316",paraId:13,tocIndex:4},{value:`\uFF1A
-\u4E00\u65E6\u6709\u5339\u914D\u7684 DOM \u53D8\u52A8\u53D1\u751F\uFF0C\u4F20\u9012\u7ED9 `,paraId:13,tocIndex:4},{value:"MutationObserver",paraId:13,tocIndex:4},{value:" \u7684\u56DE\u8C03\u51FD\u6570\u5C31\u4F1A\u88AB\u6267\u884C\u3002\u56DE\u8C03\u51FD\u6570\u4F1A\u6536\u5230\u4E00\u4E2A\u63CF\u8FF0\u6240\u6709\u53D8\u52A8\u7684 ",paraId:13,tocIndex:4},{value:"MutationRecord",paraId:13,tocIndex:4},{value:" \u5BF9\u8C61\u6570\u7EC4\uFF0C\u4EE5\u53CA\u89C2\u5BDF\u8005\u672C\u8EAB\u7684\u5F15\u7528\u3002",paraId:13,tocIndex:4},{value:"\u65AD\u5F00\u89C2\u5BDF\u8005",paraId:14,tocIndex:4},{value:`\uFF1A
-\u5F53\u4F60\u4E0D\u518D\u9700\u8981\u76D1\u89C6 DOM \u53D8\u5316\u65F6\uFF0C\u53EF\u4EE5\u8C03\u7528 `,paraId:14,tocIndex:4},{value:"observer.disconnect()",paraId:14,tocIndex:4},{value:" \u6765\u505C\u6B62\u89C2\u5BDF\u8005\uFF0C\u91CA\u653E\u8D44\u6E90\u3002",paraId:14,tocIndex:4},{value:"\u4E0B\u9762\u662F\u4E00\u4E2A\u7B80\u5355\u7684 ",paraId:15,tocIndex:4},{value:"MutationObserver",paraId:15,tocIndex:4},{value:" \u793A\u4F8B\uFF1A",paraId:15,tocIndex:4},{value:`// \u521B\u5EFA\u4E00\u4E2A\u56DE\u8C03\u51FD\u6570
+`,paraId:10,tocIndex:3},{value:`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div id="btn">Click me</div>
+  </body>
+  <script>
+    const $ =document.querySelector.bind(document);
+    const div = document.createElement('div');
+    div.id = 'container';
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.backgroundColor = 'lightblue';
+    document.body.appendChild(div);
+
+    console.log('length', document.querySelector('body').children.length);
+    alert('\u672C\u6B21 call stack \u7ED3\u675F\uFF0CDOM \u7ED3\u6784\u5DF2\u66F4\u65B0\uFF0C\u4F46\u5C1A\u672A\u89E6\u53D1\u6E32\u67D3');
+    // \uFF08alert \u4F1A\u963B\u65AD js \u6267\u884C\uFF0C\u4E5F\u4F1A\u963B\u65AD DOM \u6E32\u67D3\uFF0C\u4FBF\u4E8E\u67E5\u770B\u6548\u679C\uFF09
+    // \u5230\u6B64\uFF0C\u5373\u672C\u6B21 call stack \u7ED3\u675F\u540E\uFF08\u540C\u6B65\u4EFB\u52A1\u90FD\u6267\u884C\u5B8C\u4E86\uFF09\uFF0C\u6D4F\u89C8\u5668\u4F1A\u81EA\u52A8\u89E6\u53D1\u6E32\u67D3\uFF0C\u4E0D\u7528\u4EE3\u7801\u5E72\u9884
+
+    // \u53E6\u5916\uFF0C\u6309\u7167 event loop \u89E6\u53D1 DOM \u6E32\u67D3\u65F6\u673A\uFF0CsetTimeout \u65F6 alert \uFF0C\u5C31\u80FD\u770B\u5230 DOM \u6E32\u67D3\u540E\u7684\u7ED3\u679C\u4E86
+    setTimeout(function () {
+      alert('setTimeout \u662F\u5728\u4E0B\u4E00\u6B21 Call Stack \uFF0C\u5C31\u80FD\u770B\u5230 DOM \u6E32\u67D3\u51FA\u6765\u7684\u7ED3\u679C\u4E86');
+    });
+  <\/script>
+</html>
+`,paraId:11,tocIndex:4},{value:"MutationObserver",paraId:12,tocIndex:5},{value:" \u662F\u4E00\u4E2A Web API\uFF0C\u7528\u4E8E\u76D1\u89C6 DOM \u6811\u7684\u53D8\u5316\uFF0C\u5982\u5143\u7D20\u7684\u6DFB\u52A0\u6216\u5220\u9664\u3001\u5C5E\u6027\u7684\u53D8\u66F4\u7B49\u3002\u5F53\u8FD9\u4E9B\u53D8\u5316\u53D1\u751F\u65F6\uFF0C",paraId:12,tocIndex:5},{value:"MutationObserver",paraId:12,tocIndex:5},{value:" \u53EF\u4EE5\u5F02\u6B65\u5730\u901A\u77E5\u5F00\u53D1\u8005\uFF0C\u5141\u8BB8\u4ED6\u4EEC\u636E\u6B64\u6267\u884C\u76F8\u5173\u7684\u56DE\u8C03\u51FD\u6570\u3002\u8FD9\u79CD\u673A\u5236\u53EF\u4EE5\u7528\u4E8E\u6267\u884C\u5404\u79CD\u4EFB\u52A1\uFF0C\u6BD4\u5982\u8DDF\u8E2A DOM \u7684\u53D8\u5316\u6765\u540C\u6B65\u66F4\u65B0 UI \u7EC4\u4EF6\u6216\u8005\u6267\u884C\u67D0\u4E9B\u7279\u5B9A\u7684\u903B\u8F91\u3002",paraId:12,tocIndex:5},{value:"MutationObserver",paraId:13,tocIndex:5},{value:" \u7684\u4F7F\u7528\u6B65\u9AA4\u5982\u4E0B\uFF1A",paraId:13,tocIndex:5},{value:"\u521B\u5EFA\u4E00\u4E2A\u89C2\u5BDF\u8005\u5B9E\u4F8B",paraId:14,tocIndex:5},{value:`\uFF1A
+\u4F7F\u7528 `,paraId:14,tocIndex:5},{value:"MutationObserver",paraId:14,tocIndex:5},{value:" \u6784\u9020\u51FD\u6570\u521B\u5EFA\u4E00\u4E2A\u65B0\u7684\u89C2\u5BDF\u8005\u5B9E\u4F8B\uFF0C\u4F20\u9012\u4E00\u4E2A\u56DE\u8C03\u51FD\u6570\u7ED9\u5B83\uFF0C\u8BE5\u51FD\u6570\u5C06\u5728\u6BCF\u6B21 DOM \u53D8\u5316\u65F6\u88AB\u8C03\u7528\u3002",paraId:14,tocIndex:5},{value:"\u914D\u7F6E\u89C2\u5BDF\u8005",paraId:15,tocIndex:5},{value:`\uFF1A
+\u901A\u8FC7\u8C03\u7528 `,paraId:15,tocIndex:5},{value:"observer.observe(targetNode, config)",paraId:15,tocIndex:5},{value:" \u65B9\u6CD5\u6765\u914D\u7F6E\u89C2\u5BDF\u8005\uFF0C\u6307\u5B9A\u4F60\u60F3\u8981\u76D1\u89C6\u7684 DOM \u8282\u70B9 (",paraId:15,tocIndex:5},{value:"targetNode",paraId:15,tocIndex:5},{value:") \u4EE5\u53CA\u4F60\u611F\u5174\u8DA3\u7684\u7279\u5B9A\u7C7B\u578B\u7684 DOM \u53D8\u52A8 (",paraId:15,tocIndex:5},{value:"config",paraId:15,tocIndex:5},{value:")\u3002",paraId:15,tocIndex:5},{value:"\u54CD\u5E94 DOM \u53D8\u5316",paraId:16,tocIndex:5},{value:`\uFF1A
+\u4E00\u65E6\u6709\u5339\u914D\u7684 DOM \u53D8\u52A8\u53D1\u751F\uFF0C\u4F20\u9012\u7ED9 `,paraId:16,tocIndex:5},{value:"MutationObserver",paraId:16,tocIndex:5},{value:" \u7684\u56DE\u8C03\u51FD\u6570\u5C31\u4F1A\u88AB\u6267\u884C\u3002\u56DE\u8C03\u51FD\u6570\u4F1A\u6536\u5230\u4E00\u4E2A\u63CF\u8FF0\u6240\u6709\u53D8\u52A8\u7684 ",paraId:16,tocIndex:5},{value:"MutationRecord",paraId:16,tocIndex:5},{value:" \u5BF9\u8C61\u6570\u7EC4\uFF0C\u4EE5\u53CA\u89C2\u5BDF\u8005\u672C\u8EAB\u7684\u5F15\u7528\u3002",paraId:16,tocIndex:5},{value:"\u65AD\u5F00\u89C2\u5BDF\u8005",paraId:17,tocIndex:5},{value:`\uFF1A
+\u5F53\u4F60\u4E0D\u518D\u9700\u8981\u76D1\u89C6 DOM \u53D8\u5316\u65F6\uFF0C\u53EF\u4EE5\u8C03\u7528 `,paraId:17,tocIndex:5},{value:"observer.disconnect()",paraId:17,tocIndex:5},{value:" \u6765\u505C\u6B62\u89C2\u5BDF\u8005\uFF0C\u91CA\u653E\u8D44\u6E90\u3002",paraId:17,tocIndex:5},{value:"\u4E0B\u9762\u662F\u4E00\u4E2A\u7B80\u5355\u7684 ",paraId:18,tocIndex:5},{value:"MutationObserver",paraId:18,tocIndex:5},{value:" \u793A\u4F8B\uFF1A",paraId:18,tocIndex:5},{value:`// \u521B\u5EFA\u4E00\u4E2A\u56DE\u8C03\u51FD\u6570
 const callback = function (mutationsList, observer) {
   for (const mutation of mutationsList) {
     if (mutation.type === 'childList') {
@@ -7037,7 +7068,7 @@ const config = { attributes: true, childList: true, subtree: true };
 observer.observe(targetNode, config);
 
 // \u540E\u7EED\uFF0C\u4F60\u53EF\u4EE5\u901A\u8FC7\u8C03\u7528 observer.disconnect() \u6765\u505C\u6B62\u89C2\u5BDF
-`,paraId:16,tocIndex:4},{value:"MutationObserver",paraId:17,tocIndex:4},{value:" \u662F\u4E00\u4E2A\u5F3A\u5927\u7684\u5DE5\u5177\uFF0C\u4F46\u7531\u4E8E\u5176\u5BF9\u6027\u80FD\u7684\u5F71\u54CD\uFF0C\u5EFA\u8BAE\u4EC5\u5728\u5FC5\u8981\u65F6\u4F7F\u7528\uFF0C\u5E76\u5C3D\u91CF\u51CF\u5C11\u76D1\u89C6\u7684\u8303\u56F4\u548C\u9891\u7387\u3002\u5982\u679C\u89C2\u5BDF\u8303\u56F4\u5F88\u5E7F\u6216\u53D8\u52A8\u9891\u7E41\uFF0C\u4F1A\u589E\u52A0\u6D4F\u89C8\u5668\u7684\u5DE5\u4F5C\u8D1F\u8F7D\uFF0C\u53EF\u80FD\u5BFC\u81F4\u6027\u80FD\u95EE\u9898\u3002\u56E0\u6B64\uFF0C\u5728\u4F7F\u7528\u65F6\u9700\u8981\u6743\u8861\u529F\u80FD\u548C\u6027\u80FD\uFF0C\u786E\u4FDD\u4EE5\u6700\u4F18\u5316\u7684\u65B9\u5F0F\u4F7F\u7528 ",paraId:17,tocIndex:4},{value:"MutationObserver",paraId:17,tocIndex:4},{value:"\u3002",paraId:17,tocIndex:4},{value:`// \u76EE\u6807\u8282\u70B9
+`,paraId:19,tocIndex:5},{value:"MutationObserver",paraId:20,tocIndex:5},{value:" \u662F\u4E00\u4E2A\u5F3A\u5927\u7684\u5DE5\u5177\uFF0C\u4F46\u7531\u4E8E\u5176\u5BF9\u6027\u80FD\u7684\u5F71\u54CD\uFF0C\u5EFA\u8BAE\u4EC5\u5728\u5FC5\u8981\u65F6\u4F7F\u7528\uFF0C\u5E76\u5C3D\u91CF\u51CF\u5C11\u76D1\u89C6\u7684\u8303\u56F4\u548C\u9891\u7387\u3002\u5982\u679C\u89C2\u5BDF\u8303\u56F4\u5F88\u5E7F\u6216\u53D8\u52A8\u9891\u7E41\uFF0C\u4F1A\u589E\u52A0\u6D4F\u89C8\u5668\u7684\u5DE5\u4F5C\u8D1F\u8F7D\uFF0C\u53EF\u80FD\u5BFC\u81F4\u6027\u80FD\u95EE\u9898\u3002\u56E0\u6B64\uFF0C\u5728\u4F7F\u7528\u65F6\u9700\u8981\u6743\u8861\u529F\u80FD\u548C\u6027\u80FD\uFF0C\u786E\u4FDD\u4EE5\u6700\u4F18\u5316\u7684\u65B9\u5F0F\u4F7F\u7528 ",paraId:20,tocIndex:5},{value:"MutationObserver",paraId:20,tocIndex:5},{value:"\u3002",paraId:20,tocIndex:5},{value:`// \u76EE\u6807\u8282\u70B9
 const targetNode = document.body;
 
 // \u521B\u5EFA MutationObserver \u5B9E\u4F8B
@@ -7060,7 +7091,7 @@ const config = { childList: true, subtree: true };
 
 // \u5F00\u59CB\u89C2\u5BDF\u76EE\u6807\u8282\u70B9
 observer.observe(targetNode, config);
-`,paraId:18,tocIndex:5},{value:`// \u76EE\u6807\u8282\u70B9\uFF08\u5047\u8BBE\u6C34\u5370\u5143\u7D20\u662F\u4E00\u4E2A\u7279\u5B9A\u7684\u8282\u70B9\uFF09
+`,paraId:21,tocIndex:6},{value:`// \u76EE\u6807\u8282\u70B9\uFF08\u5047\u8BBE\u6C34\u5370\u5143\u7D20\u662F\u4E00\u4E2A\u7279\u5B9A\u7684\u8282\u70B9\uFF09
 const watermarkElement = document.querySelector('.watermark');
 
 // \u521B\u5EFA MutationObserver \u5B9E\u4F8B
@@ -7081,7 +7112,7 @@ const config = { attributes: true, subtree: true, attributeFilter: ['style'] };
 
 // \u5F00\u59CB\u89C2\u5BDF\u76EE\u6807\u8282\u70B9
 observer.observe(document.body, config);
-`,paraId:19,tocIndex:6}]},44903:function(o,n,e){e.r(n),e.d(n,{texts:function(){return a}});const a=[{value:`<!DOCTYPE html>
+`,paraId:22,tocIndex:7}]},44903:function(o,n,e){e.r(n),e.d(n,{texts:function(){return a}});const a=[{value:`<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
