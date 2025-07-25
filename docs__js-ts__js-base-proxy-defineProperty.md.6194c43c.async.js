cd1c8f2785b9484c187cee2150a3961737404290
Jerryqun <Jerryqun@users.noreply.github.com>
2025-07-25 07:53:04 +0000

    deploy: 65642bcee08db442ea4ffd7a027c559f34442897

---
+++ docs__js-ts__js-base-proxy-defineProperty.md.6194c43c.async.js
@@ -0,0 +1,85 @@
+"use strict";(self.webpackChunkblog_cq=self.webpackChunkblog_cq||[]).push([[6784],{21875:function(l,r,t){t.r(r);var d=t(72269),u=t(93359),c=t(61788),x=t(19977),j=t(91558),a=t(24268),p=t(96057),h=t(85939),o=t(53683),s=t(80936),_=t(67294),n=t(25054),e=t(85893);function i(){return(0,e.jsx)(o.dY,{children:(0,e.jsx)(_.Suspense,{fallback:(0,e.jsx)(s.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)("p",{children:(0,e.jsx)("a",{href:"https://juejin.cn/post/7069397770766909476",target:"_blank",children:n.texts[0].value})}),(0,e.jsxs)("h2",{id:"proxy",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#proxy",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Proxy"]}),(0,e.jsxs)("ol",{children:[(0,e.jsx)("li",{children:n.texts[1].value}),(0,e.jsx)("li",{children:n.texts[2].value}),(0,e.jsx)("li",{children:n.texts[3].value})]}),(0,e.jsx)("p",{children:n.texts[4].value}),(0,e.jsx)(a.Z,{lang:"js",children:n.texts[5].value}),(0,e.jsxs)("h2",{id:"reflect\u9759\u6001\u7C7B",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#reflect\u9759\u6001\u7C7B",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Reflect\uFF08\u9759\u6001\u7C7B\uFF09"]}),(0,e.jsx)("p",{children:n.texts[6].value}),(0,e.jsx)(a.Z,{lang:"js",children:n.texts[7].value}),(0,e.jsxs)("h3",{id:"reflect\u4F5C\u7528",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#reflect\u4F5C\u7528",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Reflect\u4F5C\u7528"]}),(0,e.jsxs)("ul",{children:[(0,e.jsx)("li",{children:n.texts[8].value}),(0,e.jsx)("li",{children:n.texts[9].value})]}),(0,e.jsxs)("p",{children:[n.texts[10].value,(0,e.jsx)("br",{}),n.texts[11].value,(0,e.jsx)("br",{}),n.texts[12].value,(0,e.jsx)("br",{}),n.texts[13].value]}),(0,e.jsx)(a.Z,{lang:"js",children:n.texts[14].value}),(0,e.jsxs)("h2",{id:"objectdefineproperty",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#objectdefineproperty",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Object.defineProperty()"]}),(0,e.jsxs)("p",{children:[n.texts[15].value,(0,e.jsx)("br",{}),n.texts[16].value,(0,e.jsx)("br",{}),n.texts[17].value]}),(0,e.jsx)(a.Z,{lang:"js",children:n.texts[18].value}),(0,e.jsxs)("h2",{id:"defineproperty-\u7F3A\u70B9",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#defineproperty-\u7F3A\u70B9",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"defineProperty \u7F3A\u70B9"]}),(0,e.jsxs)("p",{children:[n.texts[19].value,(0,e.jsx)("br",{}),n.texts[20].value,(0,e.jsx)("br",{}),n.texts[21].value,(0,e.jsx)("br",{}),n.texts[22].value]})]})})})})}r.default=i},25054:function(l,r,t){t.r(r),t.d(r,{texts:function(){return d}});const d=[{value:"\u53C2\u8003\u94FE\u63A5",paraId:0},{value:"\u975E\u4FB5\u5165\u7684\u5F62\u5F0F\u76D1\u542C\u5BF9\u8C61",paraId:1,tocIndex:0},{value:"\u53EF\u4EE5\u76F4\u63A5\u76D1\u542C\u6570\u7EC4\u7684\u8BFB\u5199\uFF0C\u5982\u679C\u662F Object.defineProperty \u9700\u8981\u91CD\u5199\u6570\u7EC4\u7684\u65B9\u6CD5",paraId:1,tocIndex:0},{value:"Proxy \u65E0\u6CD5\u76F4\u63A5\u76D1\u542C\u57FA\u672C\u6570\u636E\u7C7B\u578B\uFF08\u5982\u5B57\u7B26\u4E32\u3001\u6570\u5B57\u3001\u5E03\u5C14\u503C\u7B49\uFF09\uFF0C\u56E0\u4E3A\u5B83\u4EEC\u662F\u4E0D\u53EF\u53D8\u7684",paraId:1,tocIndex:0},{value:"Proxy \u53EF\u4EE5\u7406\u89E3\u6210\uFF0C\u5728\u76EE\u6807\u5BF9\u8C61\u4E4B\u524D\u67B6\u8BBE\u4E00\u5C42\u201C\u62E6\u622A\u201D\uFF0C\u5916\u754C\u5BF9\u8BE5\u5BF9\u8C61\u7684\u8BBF\u95EE\uFF0C\u90FD\u5FC5\u987B\u5148\u901A\u8FC7\u8FD9\u5C42\u62E6\u622A\uFF0C\u56E0\u6B64\u63D0\u4F9B\u4E86\u4E00\u79CD\u673A\u5236\uFF0C\u53EF\u4EE5\u5BF9\u5916\u754C\u7684\u8BBF\u95EE\u8FDB\u884C\u8FC7\u6EE4\u548C\u6539\u5199 target \u53C2\u6570\u8868\u793A\u6240\u8981\u62E6\u622A\u7684\u76EE\u6807\u5BF9\u8C61\uFF0Chandler \u53C2\u6570\u4E5F\u662F\u4E00\u4E2A\u5BF9\u8C61\uFF0C\u7528\u6765\u5B9A\u5236\u62E6\u622A\u884C\u4E3A\u3002",paraId:2,tocIndex:0},{value:`const proxy = new Proxy(target, handler);
+
+/**
+ *  \u624B\u5199\u7528 ES6proxy \u5982\u4F55\u5B9E\u73B0 arr[-1] \u7684\u8BBF\u95EE
+ */
+
+const arrProxy = (arr) =>
+  new Proxy(arr, {
+    get(target, key) {
+      key = +key;
+      while (key < 0) {
+        key += target.length;
+      }
+      return target[key];
+    },
+  });
+
+const a = arrProxy([0, 1, 2, 3]);
+console.log(a);
+`,paraId:3,tocIndex:0},{value:"Reflect \u7684\u6240\u6709\u5C5E\u6027\u548C\u65B9\u6CD5\u90FD\u662F\u9759\u6001\u7684\uFF0C\u7EDF\u4E00\u63D0\u4F9B\u4E00\u5957\u7528\u4E8E\u64CD\u4F5C\u5BF9\u8C61\u7684 api",paraId:4,tocIndex:1},{value:`const obj = {
+  name: 'zcq',
+  age: 18,
+};
+// console.log('name' in obj); => Reflect.has(obj,'name')
+// console.log(delete obj.name); => Reflect.deleteProperty(obj,'name')
+// console.log(Object.keys(obj));=> Reflect.ownKeys(obj)
+`,paraId:5,tocIndex:1},{value:"\u6240\u6709\u65B9\u6CD5\u90FD\u5728 Reflect\uFF0C \u65B9\u4FBF\u64CD\u4F5C",paraId:6,tocIndex:2},{value:"\u56E0\u4E3AProxy\u53EF\u4EE5\u6539\u5199\u9ED8\u8BA4\u7684\u539F\u751FAPI\uFF0C\u5982\u679C\u4E00\u65E6\u539F\u751FAPI\u522B\u6539\u5199\u53EF\u80FD\u5C31\u627E\u4E0D\u5230\u4E86\uFF0C\u6240\u4EE5Reflect\u4E5F\u53EF\u4EE5\u8D77\u5230\u5907\u4EFD\u539F\u751FAPI\u7684\u4F5C\u7528",paraId:6,tocIndex:2},{value:"ES6 \u4E2D\u64CD\u4F5C\u5BF9\u8C61\u800C\u63D0\u4F9B\u7684\u65B0 API\uFF0C\u82E5\u9700\u8981\u5728 Proxy \u5185\u90E8\u8C03\u7528\u5BF9\u8C61\u7684\u9ED8\u8BA4\u884C\u4E3A\uFF0C\u5EFA\u8BAE\u4F7F\u7528 Reflect",paraId:7,tocIndex:2},{value:`
+1\u3001\u53EA\u8981 Proxy \u5BF9\u8C61\u5177\u6709\u7684\u4EE3\u7406\u65B9\u6CD5\uFF0CReflect \u5BF9\u8C61\u5168\u90E8\u5177\u6709\uFF0C\u4EE5\u9759\u6001\u65B9\u6CD5\u7684\u5F62\u5F0F\u5B58\u5728`,paraId:7,tocIndex:2},{value:`
+2\u3001\u4FEE\u6539\u67D0\u4E9B Object \u65B9\u6CD5\u7684\u8FD4\u56DE\u7ED3\u679C\uFF0C\u8BA9\u5176\u53D8\u5F97\u66F4\u5408\u7406\uFF08\u5B9A\u4E49\u4E0D\u5B58\u5728\u5C5E\u6027\u884C\u4E3A\u7684\u65F6\u5019\u4E0D\u62A5\u9519\u800C\u662F\u8FD4\u56DE false\uFF09`,paraId:7,tocIndex:2},{value:`
+3\u3001\u8BA9 Object \u64CD\u4F5C\u90FD\u53D8\u6210\u51FD\u6570\u884C\u4E3A`,paraId:7,tocIndex:2},{value:`const proxy = new Proxy(obj, {
+  get(target, property) {
+    // \u5982\u679C\u6CA1\u6709\u5B9A\u4E49 get \u65B9\u6CD5\uFF0C\u90A3\u4E48\u9ED8\u8BA4\u8FD4\u56DE\u7684\u5C31\u662F Reflect \u7684 get \u65B9\u6CD5
+    return Reflect.get(target, property);
+  },
+});
+`,paraId:8,tocIndex:2},{value:"obj\uFF1A\u8981\u6DFB\u52A0\u5C5E\u6027\u7684\u5BF9\u8C61",paraId:9,tocIndex:3},{value:`
+prop\uFF1A\u8981\u5B9A\u4E49\u6216\u4FEE\u6539\u7684\u5C5E\u6027\u7684\u540D\u79F0\u6216 [Symbol]`,paraId:9,tocIndex:3},{value:`
+descriptor\uFF1A\u8981\u5B9A\u4E49\u6216\u4FEE\u6539\u7684\u5C5E\u6027\u63CF\u8FF0\u7B26`,paraId:9,tocIndex:3},{value:`Object.defineProperty(obj, prop, descriptor);
+
+const object1 = {};
+
+Object.defineProperty(object1, 'property1', {
+  // value: 42 \uFF08value \u548Cget \u4E92\u65A5\uFF09,
+  get: function (val) {
+    return 'property1Value';
+  },
+  // writable: false \uFF08 writable \u548Cset \u4E92\u65A5\uFF09
+  set: function (newVal) {},
+});
+
+object1.property1 = 77;
+// throws an error in strict mode
+console.log(object1.property1);
+// expected output: 42
+
+/**
+  configurable \u662F\u5426\u53EF\u4EE5\u5220\u9664\u5C5E\u6027\u548C\u5C5E\u6027\u63CF\u8FF0
+  enumerable \u624D\u80FD\u51FA\u73B0\u5728\u5BF9\u8C61\u679A\u4E3E\u4E2D
+  value \u521D\u59CB\u503C
+  writable \u662F\u5426\u80FD\u88AB\u8D4B\u503C\u8FD0\u7B97\u7B26\u6539\u53D8
+  get
+  set
+  */
+
+const person = { name: 'Lydia' };
+
+Object.defineProperty(person, 'age', { value: 21 });
+
+console.log(person); // { name: "Lydia", age: 21 }
+console.log(Object.keys(person)); // ['name'] \u65B0\u589E\u7684\u5C5E\u6027\u9ED8\u8BA4\u4E0D\u53EF\u679A\u4E3E
+
+person.age = 22;
+console.log(person.age); // 21  \u6CA1\u53D8
+
+/**
+ * \u901A\u8FC7defineProperty\u65B9\u6CD5\uFF0C\u6211\u4EEC\u53EF\u4EE5\u7ED9\u5BF9\u8C61\u6DFB\u52A0\u4E00\u4E2A\u65B0\u5C5E\u6027\uFF0C\u6216\u8005\u4FEE\u6539\u5DF2\u7ECF\u5B58\u5728\u7684\u5C5E\u6027\u3002
+ * \u800C\u6211\u4EEC\u4F7F\u7528defineProperty\u65B9\u6CD5\u7ED9\u5BF9\u8C61\u6DFB\u52A0\u4E86\u4E00\u4E2A\u5C5E\u6027\u4E4B\u540E\uFF0C\u5C5E\u6027\u9ED8\u8BA4\u4E3A \u4E0D\u53EF\u679A\u4E3E(not enumerable). Object.keys\u65B9\u6CD5\u4EC5\u8FD4\u56DE\u5BF9\u8C61\u4E2D
+ * \u53EF\u679A\u4E3E(enumerable) \u7684\u5C5E\u6027\uFF0C\u56E0\u6B64\u53EA\u5269\u4E0B\u4E86"name".
+ * \u7528defineProperty\u65B9\u6CD5\u6DFB\u52A0\u7684\u5C5E\u6027\u9ED8\u8BA4\u4E0D\u53EF\u53D8\u3002\u4F60\u53EF\u4EE5\u901A\u8FC7writable, configurable \u548C enumerable\u5C5E\u6027\u6765\u6539\u53D8\u8FD9\u4E00\u884C\u4E3A\u3002\u8FD9\u6837\u7684\u8BDD\uFF0C
+ * \u76F8\u6BD4\u4E8E\u81EA\u5DF1\u6DFB\u52A0\u7684\u5C5E\u6027\uFF0CdefineProperty\u65B9\u6CD5\u6DFB\u52A0\u7684\u5C5E\u6027\u6709\u4E86\u66F4\u591A\u7684\u63A7\u5236\u6743\u3002
+ */
+`,paraId:10,tocIndex:3},{value:"1\u3001\u4E0D\u80FD\u62E6\u622A\u65B0\u589E\u5C5E\u6027",paraId:11,tocIndex:4},{value:`
+2\u3001\u901A\u8FC7\u4E0B\u6807\u7684\u65B9\u5F0F\u4FEE\u6539\u6570\u7EC4\u6570\u636E\u65E0\u6CD5\u76D1\u542C`,paraId:11,tocIndex:4},{value:`
+3\u3001\u9700\u8981\u904D\u5386\u6240\u6709\u5C5E\u6027 \u6027\u80FD\u5DEE`,paraId:11,tocIndex:4},{value:`
+3\u3001vue3 \u66FF\u4EE3\u65B9\u6848\u662Fproxy   proxy\u662Fes6\u8BED\u6CD5 \u6709\u517C\u5BB9\u95EE\u9898`,paraId:11,tocIndex:4}]}}]);
